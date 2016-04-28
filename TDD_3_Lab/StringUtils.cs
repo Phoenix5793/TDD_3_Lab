69d63f85acae6bfdccdb6e9e5474bfc739d28c3c
Phoenix5793 <andreascarlen95@hotmail.com>
2016-04-28 13:47:07 +0200

    Method + class added

---
+++ TDD_3_Lab/StringUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_3_Lab
+{
+    class StringUtils
+    {
+
+        public static void CountVowelsAndConsonants(string input, out int vowelCount, out int consonantCount)
+        {
+            vowelCount = 0;
+            consonantCount = 0;
+            input = input.ToLower();
+
+            var vowels = new HashSet<Char> {'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö'};
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (vowels.Contains(input[i]))
+                {
+                    vowelCount++;
+                }
+            }
+            consonantCount = input.Length - vowelCount;
+
+        }
+
+
+
+
+    }
+}
